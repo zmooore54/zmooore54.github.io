a11a54d082bc1e2a9b3eb9c8c9b5f3379f82a5f4
Zora Moore <zora.oliva54@gmail.com>
2019-01-02 20:23:33 -0500

    1_2_19 update

---
+++ JS/functions.js
@@ -0,0 +1,66 @@
+
+
+function price() {
+
+	var x = document.getElementById("price");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    else {
+        x.style.display = "none";
+    }
+}
+
+
+function cuisine() {
+    var x = document.getElementById("cuisine");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    else {
+        x.style.display = "none";
+    }
+}
+
+function type1() {
+    var x = document.getElementById("type1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    else {
+        x.style.display = "none";
+    }
+}
+
+function cation() {
+    var x = document.getElementById("cation");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    else {
+        x.style.display = "none";
+    }
+}
+
+
+
+function menu() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    else {
+        x.style.display = "none";
+    }
+}
+
+function rating() {
+    var x = document.getElementById("rating");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } 
+    else {
+        x.style.display = "none";
+    }
+}
+
---
+++ JS/hoppes_mt_01.js
@@ -0,0 +1,6 @@
+var base_amount = document.getElementById("BASE").value
+
+document.getElementById("base_a").innerHTML = "The base amount is " + base_amount +;
+document.getElementById("tax_a").innerHTML = "The base amount is " + number +;
+document.getElementById("tip_a").innerHTML = "The base amount is " + number +;
+document.getElementById("tot_a").innerHTML = "The base amount is " + number +;

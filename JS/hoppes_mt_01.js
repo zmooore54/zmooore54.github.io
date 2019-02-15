cd404f5bb5d9ef91171102a79564efffc8b8c033
Zora Moore <zora.oliva54@gmail.com>
2019-02-15 01:35:47 -0500

    2/15/19
    redesing of front page

--- JS/hoppes_mt_01.js
+++
@@ -1,6 +0,0 @@
-var base_amount = document.getElementById("BASE").value
-
-document.getElementById("base_a").innerHTML = "The base amount is " + base_amount +;
-document.getElementById("tax_a").innerHTML = "The base amount is " + number +;
-document.getElementById("tip_a").innerHTML = "The base amount is " + number +;
-document.getElementById("tot_a").innerHTML = "The base amount is " + number +;
---
+++ JS/jsfunctions.js
@@ -0,0 +1,47 @@
+// return to top
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+//side nav
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+}
+
+//drop down
+
+var dropdown = document.getElementsbyClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+	dropdown[i].addEventListener("click", function() {
+	this.classList.toggle("active");
+	var dropdownContent = this.nextElementSibling;
+	if (dropdownContent.style.display === "block") {
+	dropdownContent.style.display = "none";
+	} else {
+	dropdownContent.style.display = "block";
+	}
+	});
+}

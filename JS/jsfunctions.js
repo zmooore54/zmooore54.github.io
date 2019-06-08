1f9084b31f98d9bae8d79b58acd73994bd97d867
Zora Moore <zora.oliva54@gmail.com>
2019-06-08 02:14:29 -0700

    june 8 update

--- JS/jsfunctions.js
+++ JS/jsfunctions.js
@@ -16,6 +16,16 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+//overlay Menu
+<div id="myNav" class="overlay">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <div class="overlay-content">
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+  </div>
+</div>
 
 //side nav
 
